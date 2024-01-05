3af72193826158a7d604840fd513eb9b5e51abb2
catiscute777 <150596081+catiscute777@users.noreply.github.com>
2024-01-05 16:37:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+     Button btn ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setText("Click me");
+
+    }
+
+    public void go(View view)
+    {
+        btn.setText("oh,yea, i've been clicked! ");
+    }
+}
